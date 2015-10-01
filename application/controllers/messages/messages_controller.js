b3e9b4f7b1327a95a53050e5b1acfb160bae881f
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-10-01 16:25:34 +0800

    feature messages controller and home content.

--- application/controllers/messages/messages_controller.js
+++ application/controllers/messages/messages_controller.js
@@ -30,4 +30,19 @@ application.controller('MessagesController', ['$location', '$mdSidenav', '$scope
         {
             $mdSidenav('friends').open();
         };
+        
+        $scope.todoList = [{todoText:'Clean House', done:false}];
+
+        $scope.todoAdd = function() {
+            $scope.todoList.push({todoText:$scope.todoInput, done:false});
+            $scope.todoInput = "";
+        };
+
+        $scope.remove = function() {
+            var oldList = $scope.todoList;
+            $scope.todoList = [];
+            angular.forEach(oldList, function(x) {
+                if (!x.done) $scope.todoList.push(x);
+            });
+        };
     }]);
