a5b7caa93b2ee747ad52da3e06eca3499387b7d3
mohammadhafiz <mohammadhafizamiruddin@gmail.com>
2015-10-02 11:45:11 +0800

    Feature for moment content.

--- application/controllers/home_controller.js
+++ application/controllers/home_controller.js
@@ -17,4 +17,56 @@ application.controller('HomeController', ['$scope', '$location',
             $location.path('/messages');
         };
         
+        var imagePath = 'img/list/60.jpeg';
+            $scope.messages = [
+              {
+                face : imagePath,
+                name: 'hafiz',
+                times: '7.45 A.M',
+                dates: '2 October 2015',
+                notes: " testing"
+              },
+              {
+                face : imagePath,
+                name: 'wahab',
+                times: '7.45 A.M',
+                dates: '2 October 2015',
+                notes: " nevermind la."
+              },
+              {
+                face : imagePath,
+                name: 'tira',
+                times: '7.45 A.M',
+                dates: '2 October 2015',
+                notes: " stay away"
+              },
+              {
+                face : imagePath,
+                name: 'limin',
+                times: '7.45 A.M',
+                dates: '2 October 2015',
+                notes: " nice to know you"
+              },
+              {
+                face : imagePath,
+                name: 'apek',
+                times: '7.45 A.M',
+                dates: '2 October 2015',
+                notes: " it's time for you and me"
+              },
+              {
+                face : imagePath,
+                name: 'lina',
+                times: '7.45 A.M',
+                dates: '2 October 2015',
+                notes: " rampage"
+              },
+              {
+                face : imagePath,
+                name: 'mickey',
+                times: '7.45 A.M',
+                dates: '2 October 2015',
+                notes: " and the end"
+              },
+            ];
     }]);
